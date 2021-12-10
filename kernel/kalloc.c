e7e54b522c51ce04fc1ace5dc1e25b95254febaf
Qingao Chai <chaiqingao@outlook.com>
2021-12-10 15:21:54 +0800

    :new: my solution for syscall lab exercise 2: sysinfo

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -80,3 +80,16 @@ kalloc(void)
     memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
 }
+
+uint64
+getfreemem(void)
+{
+  struct run *r;
+  uint64 freemem = 0;
+  acquire(&kmem.lock);
+  for (r = kmem.freelist; r; r = r->next)
+    freemem++;
+  release(&kmem.lock);
+  freemem *= 4096;
+  return freemem;
+}
--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -6,6 +6,7 @@
 #include "memlayout.h"
 #include "spinlock.h"
 #include "proc.h"
+#include "sysinfo.h"
 
 uint64
 sys_exit(void)
@@ -103,5 +104,17 @@ sys_trace(void)
   if(argint(0, &tracemask) < 0)
     return -1;
   myproc()->tracemask = tracemask;
+  return 0;
+}
+
+uint64
+sys_sysinfo(void)
+{
+  uint64 addr;
+  if (argaddr(0, &addr) < 0)
+    return -1;
+  struct sysinfo info = {getfreemem(), getnproc()};
+  if (copyout(myproc()->pagetable, addr, (char *)&info, sizeof(info)) < 0)
+    return -1;
   return 0;
 }
